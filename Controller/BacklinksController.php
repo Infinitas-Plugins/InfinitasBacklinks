9443d6789687b8f8d677ac494f6f8196bf5b6ae6
dogmatic69 <dogmatic69@gmail.com>
2012-02-15 16:04:32 +0000

    removing the filter helper as it is loaded by the core

--- Controller/BacklinksController.php
+++ Controller/BacklinksController.php
@@ -4,13 +4,9 @@
 	 *
 	 */
 	class BacklinksController extends BacklinksAppController{
-		var $name = 'Backlinks';
+		public $name = 'Backlinks';
 
-		public $helpers = array(
-			'Filter.Filter'
-		);
-
-		function admin_index(){
+		public function admin_index(){
 			$address = env('SERVER_ADDR');
 			if($address == '127.0.0.1' || $address == '::1'){
 				$this->notice(
