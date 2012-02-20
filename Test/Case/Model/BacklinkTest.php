58f0436822c852408443328931a064af0c4c2a62
Ceeram <c33ram@gmail.com>
2012-02-20 12:33:40 +0100

    backlinks tests

--- Test/Case/Model/BacklinkTest.php
+++ Test/Case/Model/BacklinkTest.php
@@ -1,11 +1,16 @@
 <?php
 /* Backlink Test cases generated on: 2010-03-13 11:03:57 : 1268471097*/
-App::import('Model', 'Backlinks.Backlink');
+App::uses('Backlink', 'Backlinks.Model');
 
 class BacklinkTestCase extends CakeTestCase {
 	function startTest() {
-		$this->Backlink =& ClassRegistry::init('Backlink');
+		$this->Backlink =& ClassRegistry::init(array(
+			'class' => 'Backlinks.Backlink',
+			'ds' => 'test'
+		));
 	}
+
+	function testDummy() {}
 
 	function endTest() {
 		unset($this->Backlink);
