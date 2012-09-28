8cf9bc5d1e997ca01b930ff71905d2ef9d9d7a6d
dogmatic69 <dogmatic69@gmail.com>
2012-09-29 00:38:25 +0100

    adding tests

---
+++ Test/Case/Lib/BacklinksEventsTest.php
@@ -0,0 +1,15 @@
+<?php
+App::uses('BacklinksEvents', 'Backlinks.Lib');
+class BacklinksEventsTest extends CakeTestCase {
+	public function setUp() {
+		parent::setUp();
+	}
+
+	public function tearDown() {
+		parent::tearDown();
+	}
+
+	public function testSomething() {
+
+	}
+}
