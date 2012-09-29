2b610575ad20ba49209d217299a8ff90e9c8e0a0
dogmatic69 <dogmatic69@gmail.com>
2012-09-29 16:28:26 +0100

    sorting out the tests for backlinks events

--- Lib/BacklinksEvents.php
+++ Lib/BacklinksEvents.php
@@ -1,30 +1,44 @@
 <?php
-	final class BacklinksEvents extends AppEvents {
-		public function onPluginRollCall() {
-			return array(
-				'name' => 'Backlinks',
-				'description' => 'See who is linking back to your site',
-				'icon' => '/Backlinks/img/icon.png',
-				'author' => 'Infinitas'
-			);
-		}
-
-		public function onSetupCache() {
-		}
-
-		public function onAdminMenu($event) {
-			$menu['main'] = array(
-				'Backlinks' => array('controller' => false, 'action' => false)
-			);
-
-			return $menu;
-		}
-
-		public function onRequireDatabaseConfigs($event) {
-			return array(
-				'backlink' => array(
-					'datasource' => 'Libs.XmlSource'
-				)
-			);
-		}
+class BacklinksEvents extends AppEvents {
+/**
+ * @brief get the plugins details
+ */
+	public function onPluginRollCall() {
+		return array(
+			'name' => 'Backlinks',
+			'description' => 'See who is linking back to your site',
+			'icon' => '/Backlinks/img/icon.png',
+			'author' => 'Infinitas'
+		);
 	}
+
+/**
+ * @brief set up plugin cache
+ */
+	public function onSetupCache() {
+	}
+
+/**
+ * @brief build the admin menu
+ */
+	public function onAdminMenu($event) {
+		$menu = array(
+			'main' => array(
+				'Backlinks' => array('controller' => false, 'action' => false)
+			)
+		);
+
+		return $menu;
+	}
+
+/**
+ * @brief load additional db configs
+ */
+	public function onRequireDatabaseConfigs($event) {
+		return array(
+			'backlink' => array(
+				'datasource' => 'Libs.XmlSource'
+			)
+		);
+	}
+}
--- Test/Case/Lib/BacklinksEventsTest.php
+++ Test/Case/Lib/BacklinksEventsTest.php
@@ -1,15 +1,23 @@
 <?php
-App::uses('BacklinksEvents', 'Backlinks.Lib');
-class BacklinksEventsTest extends CakeTestCase {
-	public function setUp() {
-		parent::setUp();
-	}
+/**
+ * @brief tests for backlinks
+ *
+ * This class extends from the InfinitasEventTestCase which does a lot of
+ * automated testing of events to reduce the amount of repetition.
+ *
+ * @see InfinitasEventTestCase
+ */
+App::uses('InfinitasEventTestCase', 'Events.Test/Lib');
 
-	public function tearDown() {
-		parent::tearDown();
-	}
+class BacklinksEventsTest extends InfinitasEventTestCase {
+/**
+ * @brief test admin menu is correct
+ */
+	public function testAdminMenu() {
+		$expected = $this->_manualCall('adminMenu', $this->ObjectEvent);
+		$this->assertTrue(isset($expected['adminMenu']['Backlinks']['main']['Backlinks']));
+		$this->assertTrue(count($expected['adminMenu']['Backlinks']['main']) == 1);
 
-	public function testSomething() {
-
+		parent::testAdminMenu();
 	}
 }
