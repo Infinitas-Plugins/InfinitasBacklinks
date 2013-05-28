4cb1a0b2ba320c111942cb83e1c0749318b6cdbb
dogmatic69 <dogmatic69@gmail.com>
2013-05-28 01:35:57 +0100

    fixing XML usage

--- Controller/BacklinksController.php
+++ Controller/BacklinksController.php
@@ -1,23 +1,20 @@
 <?php
-	/**
-	 *
-	 *
-	 */
-	class BacklinksController extends BacklinksAppController{
-		public $name = 'Backlinks';
+/**
+ * BacklinksController
+ */
 
-		public function admin_index() {
-			$address = env('SERVER_ADDR');
-			if($address == '127.0.0.1' || $address == '::1') {
-				$this->notice(
-					__('This plugin does not work on localhost'),
-					array(
-						'level' => 'warning'
-					)
-				);
-			}
+class BacklinksController extends BacklinksAppController {
 
-			$data = $this->Backlink->find('all');
-			$this->set(compact('data'));
+	public function admin_index() {
+		$address = env('SERVER_ADDR');
+		if($address == '127.0.0.1' || $address == '::1') {
+			$this->notice(__('This plugin does not work on localhost'), array(
+				'level' => 'warning'
+			));
 		}
+
+		$data = $this->Backlink->find('all');
+		$this->set(compact('data'));
 	}
+
+}
--- Model/Backlink.php
+++ Model/Backlink.php
@@ -1,89 +1,87 @@
 <?php
-	/**
-	 *
-	 *
-	 */
-	class Backlink extends BacklinksAppModel{
-		var $name = 'Backlink';
+/**
+ * Backlink
+ */
+class Backlink extends BacklinksAppModel {
 
-		var $useDbConfig = 'backlink';
+	public $useDbConfig = 'backlink';
 
-		var $actsAs = false;
+	public $actsAs = false;
 
-		var $useTable = false;
+	public $useTable = false;
 
-		var $schema = array(
-			'title' => array(
-				'type' => 'string',
-				'null' => true,
-				'key' => 'primary',
-				'length' => 255
-			),
-			'link' => array(
-				'type' => 'string',
-				'null' => true,
-				'key' => 'primary',
-				'length' => 255
-			),
-			'description' => array(
-				'type' => 'text',
-				'null' => true,
-				'key' => 'primary',
-				'length' => null
-			),
-			'publisher' => array(
-				'type' => 'string',
-				'null' => true,
-				'key' => 'primary',
-				'length' => 255
-			),
-			'creator' => array(
-				'type' => 'string',
-				'null' => true,
-				'key' => 'primary',
-				'length' => 255
-			),
-			'date' => array(
-				'type' => 'string',
-				'null' => true,
-				'key' => 'primary',
-				'length' => 255
+	public $schema = array(
+		'title' => array(
+			'type' => 'string',
+			'null' => true,
+			'key' => 'primary',
+			'length' => 255
+		),
+		'link' => array(
+			'type' => 'string',
+			'null' => true,
+			'key' => 'primary',
+			'length' => 255
+		),
+		'description' => array(
+			'type' => 'text',
+			'null' => true,
+			'key' => 'primary',
+			'length' => null
+		),
+		'publisher' => array(
+			'type' => 'string',
+			'null' => true,
+			'key' => 'primary',
+			'length' => 255
+		),
+		'creator' => array(
+			'type' => 'string',
+			'null' => true,
+			'key' => 'primary',
+			'length' => 255
+		),
+		'date' => array(
+			'type' => 'string',
+			'null' => true,
+			'key' => 'primary',
+			'length' => 255
+		)
+	);
+
+	public $request = array(
+		'method' => 'GET',
+		'uri' => array(
+			'scheme' => 'http',
+			'host' => 'blogsearch.google.com',
+			'path' => '/blogsearch_feeds',
+			'query' => array(
+				'scoring' => 'd',
+				'ie' => 'utf-8',
+				'num' => 20,
+				'output' => 'rss',
+				'partner' => 'infinitas',
+				'q' => 'link:%site%'
 			)
-		);
-
-		var $request = array(
-			'method' => 'GET',
-			'uri' => array(
-				'scheme' => 'http',
-				'host' => 'blogsearch.google.com',
-				'path' => '/blogsearch_feeds',
-				'query' => array(
-					'scoring' => 'd',
-					'ie' => 'utf-8',
-					'num' => 20,
-					'output' => 'rss',
-					'partner' => 'infinitas',
-					'q' => 'link:%site%'
-				)
-			)
-		);
+		)
+	);
 
 
-		var $map = array(
-			'count'  => 'Rss.Channel.totalResults',
-			'limit'  => 'Rss.Channel.itemsPerPage',
-			'page'   => 'Rss.Channel.startIndex',
-			'data'   => 'Rss.Channel.Item'
-		);
+	public $map = array(
+		'count'  => 'Rss.Channel.totalResults',
+		'limit'  => 'Rss.Channel.itemsPerPage',
+		'page'   => 'Rss.Channel.startIndex',
+		'data'   => 'Rss.Channel.Item'
+	);
 
-		function __construct($id = false, $table = null, $ds = null) {
-			parent::__construct($id, $table, $ds);
+	function __construct($id = false, $table = null, $ds = null) {
+		parent::__construct($id, $table, $ds);
 
-			/**
-			 * router is not loaded.
-			 */
-			if(php_sapi_name() != 'cli') {
-				$this->request['uri']['query']['q'] = str_replace('%site%', Router::url('/', true), $this->request['uri']['query']['q']);
-			}
+		/**
+		 * router is not loaded.
+		 */
+		if(php_sapi_name() != 'cli') {
+			$this->request['uri']['query']['q'] = str_replace('%site%', Router::url('/', true), $this->request['uri']['query']['q']);
 		}
 	}
+}
